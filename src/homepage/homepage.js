90a58f6287cc55b7f3b6388efda2867fdad856a3
samarqureshii <samarq713@gmail.com>
2023-04-05 00:46:49 -0400

    updated homepage

---
+++ src/homepage/homepage.js
@@ -0,0 +1,93 @@
+anime({
+    targets:'#Car_1',
+    duration:3000,
+    opacity:{
+        value:[0,1],
+        duration:500
+    },
+    translateX:[-27,126],
+    translateY:[15,-70],
+    easing:"linear",
+    loop:true
+})
+
+setTimeout(function(){
+    anime({
+        targets:'#Car_3',
+        duration:3000,
+        opacity:{
+            value:[0,1],
+            duration:500
+        },
+        translateX:[-126,27],
+        translateY:[70,-15],
+        easing:"linear",
+        loop:true
+    })
+},1500)
+setTimeout(function(){
+    anime({
+        targets:'#Lorry_1',
+        duration:3000,
+        opacity:{
+            value:[0,1],
+            duration:500
+        },
+        translateX:[-90,63],
+        translateY:[50,-35],
+        easing:"linear",
+        loop:true
+    })
+},700)
+anime({
+    targets:'#Car_2',
+    duration:2000,
+    opacity:{
+        value:[1,0],
+        duration:500,
+        delay:1500
+    },
+    
+    translateX:-54,
+    translateY:-30,
+    easing:"linear",
+    loop:true
+})
+anime({
+    targets:'#Car_4',
+    duration:2000,
+    opacity:{
+        value:[0,1],
+        duration:500
+    },
+    translateX:[18,-63],
+    translateY:[10,-35],
+    easing:"linear",
+    loop:true
+})
+anime({
+    targets:'#Car_5',
+    duration:2000,
+    opacity:{
+        value:[0,1],
+        duration:500
+    },
+    translateX:[18,-63],
+    translateY:[10,-35],
+    easing:"linear",
+    loop:true
+})
+
+    anime({
+        targets:'#Lorry_2',
+        duration:3000,
+        opacity:{
+            value:[0,1],
+            duration:500
+        },
+        translateX:[9,-63],
+        translateY:[-5,35],
+        easing:"linear",
+        loop:true,
+        delay:500
+    })
